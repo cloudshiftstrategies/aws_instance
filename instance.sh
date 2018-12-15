548410d6e05800e7902d1feb5e12f48ddc7ea445
Brian Peterson <brian@petersons.com>
2018-12-15 13:42:48 -0600

    updates

--- instance.sh
+++ instance.sh
@@ -8,19 +8,20 @@
 set -e
 
 print_usage() {
-    echo "USAGE: $0 launch|terminate [ubuntu|aws|aws2]"
+    echo "USAGE: $0 launch|list|terminate [ubuntu|aws|aws2]"
 }
 
 SIZE=t2.micro
 LOCAL_KEY=~/.ssh/id_rsa.pub
 INSTANCES_LOG=./instances.log
+SG_NAME=ssh-only-sg
 
 case $1 in 
     launch )
         echo "Launching (1) $2 instance size: $SIZE in $AWS_DEFAULT_REGION"
         case $2 in 
             ubuntu )
-                NAME=ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-*-amd64-server*
+                NAME=ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server*
                 USER=ubuntu
                 ;;
             aws )
@@ -42,6 +43,8 @@ case $1 in
             --query 'sort_by(Images, &CreationDate)[-1].ImageId' \
             | sed s/\"//g`
         echo "found: $AMI"
+
+        # Find or create the ssh keypair
         HOSTNAME=`uname -n`
 
         if [ `aws ec2 describe-key-pairs | jq .KeyPairs[].KeyName | grep -c $HOSTNAME` -lt 1 ]; then
@@ -49,18 +52,54 @@ case $1 in
                 echo "Creating local ssh keypair"
                 ssh-keygen -f id_rsa -t rsa -N ''
             fi
-            "Importing local keypair ~/.ssh/id_rsa.pub into aws keypair name $HOSTNAME"
+            echo "Importing local public key: $LOCAL_KEY into aws as key-name: $HOSTNAME"
             aws ec2 import-key-pair \
                 --key-name $HOSTNAME \
-                --public-key-material file://$LOCAL_KEY
+                --public-key-material file://$LOCAL_KEY > /dev/null
         else
             echo "Using existing AWS key name: $HOSTNAME"
         fi
+
+        # Find the default vpc
+        DEFAULT_VPC=`aws ec2 describe-vpcs | jq -r '.Vpcs[] | select(.IsDefault==true) | .VpcId'`
+
+        # Find or create the security group ssh-only
+        if [ `aws ec2 describe-security-groups --filter Name=vpc-id,Values=$DEFAULT_VPC | jq -r .SecurityGroups[].GroupName | grep -c $SG_NAME` -lt 1 ]; then
+            # The security group doesnt exist.. create it
+            echo "Creating security group $SG_NAME in default vpc: $DEFAULT_VPC"
+            aws ec2 create-security-group \
+                --group-name $SG_NAME \
+                --description "SSH Only Security Group for default vpc" \
+                --vpc-id $DEFAULT_VPC > /dev/null
+        fi
+        # Lookup security Group ID with $SG_NAME and in default VPC
+        SG_ID=`aws ec2 describe-security-groups \
+            --filter Name=group-name,Values=$SG_NAME \
+            --filter Name=vpc-id,Values=$DEFAULT_VPC \
+            | jq -r .SecurityGroups[0].GroupId`
+        echo "Found Security Group name: $SG_NAME as ID : $SG_ID"
+
+        # Add rule to security group
+        MY_IP="`curl -s https://ipinfo.io/ip`/32"
+        if [ -n `aws ec2 describe-security-groups \
+            --filter Name=group-name,Values=ssh-only-sg \
+            | jq --arg ip "$MY_IP" '.SecurityGroups[0].IpPermissions[] | select((.FromPort==22) and (.ToPort=22) and (.IpProtocol=="tcp") and (.IpRanges[0].CidrIp==$ip)) | length'` ]; then
+            echo "Adding inbound ssh rule from IP $MY_IP to Security Group: $SG_NAME"
+            aws ec2 authorize-security-group-ingress \
+                --group-id $SG_ID \
+                --protocol tcp \
+                --port 22 \
+                --cidr $MY_IP
+        else
+            echo "Inbound ssh rule from IP $MY_IP to Security Group: $SG_NAME already exists"
+        fi
+
         # Launch the instance
         echo -n "Launching instance id: "
         INSTANCE=`aws ec2 run-instances \
             --image-id $AMI \
             --instance-type t2.micro \
+            --security-groups $SG_NAME \
             --key-name $HOSTNAME`
         # Get the instance ID
         INSTANCE_ID=`echo $INSTANCE | jq -r .Instances[].InstanceId`
@@ -79,12 +118,22 @@ case $1 in
         echo "Connect with: ssh -i $LOCAL_KEY $USER@$PUBLIC_IP"
         ;;
 
+    list )
+        # Get Instance public IP(s)
+        for INSTANCE_ID in `cat instances.log`; do
+            echo -n "$INSTANCE_ID : "
+            aws ec2 describe-instances \
+                --instance-id $INSTANCE_ID \
+                | jq -r .Reservations[0].Instances[0].PublicIpAddress
+        done
+        ;;
+
     terminate )
         # Look up the list of instances created with this tool
         for INSTANCE_ID in `cat instances.log`; do
             # Terminate each
             echo "Terminating $INSTANCE_ID"
-            aws ec2 terminate-instances --instance-id $INSTANCE_ID
+            aws ec2 terminate-instances --instance-id $INSTANCE_ID > /dev/null
         done
         # Remove the instances.log file
         echo "Removing instances log: $INSTANCES_LOG"
